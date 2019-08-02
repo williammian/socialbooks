571c49ecd800b1ba4fcd8ca42da2cb87be81475e
William Mian <william_mian@yahoo.com.br>
2019-08-02 09:22:47 -0300

    modelando recurso

--- socialbooksapi/src/test/java/br/com/wm/socialbooks/SocialbooksapiApplicationTests.java
+++ socialbooksapi/src/test/java/br/com/wm/socialbooks/SocialbooksapiApplicationTests.java
@@ -2,11 +2,13 @@ package br.com.wm.socialbooks;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SocialbooksapiApplication.class)
+@WebAppConfiguration
 public class SocialbooksapiApplicationTests {
 
 	@Test
