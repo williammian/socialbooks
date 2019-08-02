ef03e6e0a77b0a77aeea39036a8fa5e55c8513b9
William Mian <william_mian@yahoo.com.br>
2019-08-02 13:26:30 -0300

    @JsonInclude

--- socialbooksapi/src/main/java/br/com/wm/socialbooks/domain/Livro.java
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/domain/Livro.java
@@ -3,20 +3,29 @@ package br.com.wm.socialbooks.domain;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class Livro {
 	
+	@JsonInclude(Include.NON_NULL)
 	private Long id;
 	
 	private String nome;
 	
+	@JsonInclude(Include.NON_NULL)
 	private Date publicacao;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String editora;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String resumo;
 	
+	@JsonInclude(Include.NON_NULL)
 	private List<Comentario> comentarios;
 	
+	@JsonInclude(Include.NON_NULL)
 	private String autor;
 	
 	public Livro() {}
