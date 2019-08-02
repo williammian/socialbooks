cdacd1abb4a80d5eedd945cef418b6330a38b2d2
William Mian <william_mian@yahoo.com.br>
2019-08-02 14:45:11 -0300

    buscando livro com @PathVariable

--- socialbooksapi/src/main/java/br/com/wm/socialbooks/resources/LivrosResources.java
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package br.com.wm.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +27,11 @@ public class LivrosResources {
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro) {
 		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Livro buscar(@PathVariable("id") Long id) {
+		return livrosRepository.findOne(id);
 	}
 
 }
