98e0ce49d067ef84863a8cc0aa042479f499153a
William Mian <william_mian@yahoo.com.br>
2019-08-02 09:51:21 -0300

    criando representacao para recurso livro

--- socialbooksapi/src/main/java/br/com/wm/socialbooks/resources/LivrosResources.java
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/resources/LivrosResources.java
@@ -1,15 +1,26 @@
 package br.com.wm.socialbooks.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.wm.socialbooks.domain.Livro;
 
 @RestController
 public class LivrosResources {
 	
 	@RequestMapping(value = "/livros", method = RequestMethod.GET)
-	public String listar() {
-		return "Rest aplicado, Git passo-a-passo";
+	public List<Livro> listar() {
+		
+		Livro l1 = new Livro("Rest aplicado");
+		Livro l2 = new Livro("Git passo-a-passo");
+		
+		Livro[] livros = {l1, l2};
+		
+		return Arrays.asList(livros);
 	}
 
 }
