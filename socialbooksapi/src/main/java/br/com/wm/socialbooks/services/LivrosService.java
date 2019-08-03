2d731347c4ad12375e5f9d143921c8cad6a74bf9
williammian <william_mian@yahoo.com.br>
2019-08-03 16:26:02 -0300

    melhorando design do código (Service)

---
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/services/LivrosService.java
@@ -0,0 +1,55 @@
+package br.com.wm.socialbooks.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import br.com.wm.socialbooks.domain.Livro;
+import br.com.wm.socialbooks.repository.LivrosRepository;
+import br.com.wm.socialbooks.services.exceptions.LivroNaoEncontradoException;
+
+@Service
+public class LivrosService {
+	
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	public List<Livro> listar() {
+		return livrosRepository.findAll();
+	}
+	
+	public Livro buscar(Long id) {
+		Livro livro = livrosRepository.findOne(id);
+		
+		if(livro == null) {
+			throw new LivroNaoEncontradoException("O livro não pôde ser encontrado.");
+		}
+		
+		return livro;
+	}
+	
+	public Livro salvar(Livro livro) {
+		livro.setId(null);
+		return livrosRepository.save(livro);
+	}
+	
+	public void deletar(Long id) {
+		try {
+			livrosRepository.delete(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new LivroNaoEncontradoException("O livro não pôde ser encontrado.");
+		}
+	}
+	
+	public void atualizar(Livro livro) {
+		verificarExistencia(livro);
+		livrosRepository.save(livro);
+	}
+	
+	private void verificarExistencia(Livro livro) {
+		buscar(livro.getId());
+	}
+
+}
