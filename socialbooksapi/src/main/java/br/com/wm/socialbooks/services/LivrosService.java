8d7051b848bdf1ee8d86ff9d54bf1f8181862001
William Mian <william_mian@yahoo.com.br>
2019-08-05 15:58:15 -0300

    listando comentários do recurso livro

--- socialbooksapi/src/main/java/br/com/wm/socialbooks/services/LivrosService.java
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/services/LivrosService.java
@@ -66,5 +66,11 @@ public class LivrosService {
 		
 		return comentariosRepository.save(comentario);
 	}
+	
+	public List<Comentario> listarComentarios(Long livroId) {
+		Livro livro = buscar(livroId);
+		
+		return livro.getComentarios();
+	}
 
 }
