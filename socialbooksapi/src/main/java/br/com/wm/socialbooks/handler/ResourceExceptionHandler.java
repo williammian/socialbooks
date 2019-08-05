d36d6a7ee8ae3c9c6bbee489ba87d294a48977c6
williammian <william_mian@yahoo.com.br>
2019-08-04 23:29:07 -0300

    manipulando erro com @ExceptionHandler e @ControllerAdvice

---
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/handler/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package br.com.wm.socialbooks.handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.wm.socialbooks.domain.DetalhesErro;
+import br.com.wm.socialbooks.services.exceptions.LivroNaoEncontradoException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(LivroNaoEncontradoException.class)
+	public ResponseEntity<DetalhesErro> handleLivroNaoEncontradoException
+							(LivroNaoEncontradoException e, HttpServletRequest request) {
+		
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(404l);
+		erro.setTitulo("O livro não pôde ser encontrado");
+		erro.setMensagemDesenvolvedor("http://erros.socialbooks.com/404");
+		erro.setTimestamp(System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+}
