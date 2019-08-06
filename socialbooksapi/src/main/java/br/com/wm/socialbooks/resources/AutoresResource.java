184c5afe6f0e601929055f7db6e9f84b49376364
William Mian <william_mian@yahoo.com.br>
2019-08-06 14:04:24 -0300

    negociação de conteúdos com Media Type

--- socialbooksapi/src/main/java/br/com/wm/socialbooks/resources/AutoresResource.java
+++ socialbooksapi/src/main/java/br/com/wm/socialbooks/resources/AutoresResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,7 +26,9 @@ public class AutoresResource {
 	@Autowired
 	private AutoresService autoresService;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE
+	})
 	public ResponseEntity<List<Autor>> listar() {
 		List<Autor> autores = autoresService.listar();
 		return ResponseEntity.status(HttpStatus.OK).body(autores);
