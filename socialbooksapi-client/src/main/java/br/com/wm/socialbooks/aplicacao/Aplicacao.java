de82ef6ccce33e0761de08de5d949a3caa581b03
William Mian <william_mian@yahoo.com.br>
2019-08-08 09:07:59 -0300

    cliente java

---
+++ socialbooksapi-client/src/main/java/br/com/wm/socialbooks/aplicacao/Aplicacao.java
@@ -0,0 +1,27 @@
+package br.com.wm.socialbooks.aplicacao;
+
+import java.net.URI;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		RestTemplate restTemplate = new RestTemplate();
+		
+		RequestEntity<Void> request = RequestEntity
+				.get(URI.create("http://localhost:8080/livros"))
+				.header("Authorization", "Basic dGVzdGU6czNuaDQ=").build();
+		
+		ResponseEntity<Livro[]> response = restTemplate.exchange(request, Livro[].class);
+		
+		for(Livro livro : response.getBody()) {
+			System.out.println("Livro: " + livro.getNome());
+		}
+		
+	}
+	
+}
--- socialbooksapi-client/src/main/java/br/com/wm/socialbooks/aplicacao/Teste.java
+++
@@ -1,5 +0,0 @@
-package br.com.wm.socialbooks.aplicacao;
-
-public class Teste {
-
-}
