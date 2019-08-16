258db3e860b975ebd76c241f8128ca1a5bd2d532
William Mian <william_mian@yahoo.com.br>
2019-08-16 16:59:13 -0300

    cliente JavaScript

---
+++ socialbooksapi-client/src/main/resources/livrosClient.js
@@ -0,0 +1,31 @@
+$(function(){
+	$(".js-load-books").on('click', function(){
+		$.ajax({
+				url: "http://localhost:8080/livros",
+				type: "get",
+				headers: {
+					"Authorization" : "Basic dGVzdGU6czNuaDQ="
+				},
+				success: function(response) {
+					desenhaTabela(response);
+				}
+		});
+	})
+});
+
+function desenhaTabela(dados) {
+	$(".js-books-table-body tr").remove();
+	for(var i=0; i < dados.length; i++) {
+		desenhaLinha(dados[i]);
+	}
+}
+
+function desenhaLinha(linha) {
+	var linhaTabela = $("<tr/>");
+	$(".js-books-table-body").append(linhaTabela);
+	linhaTabela.append("<td>" + linha.id + "</td>");
+	linhaTabela.append("<td>" + linha.nome + "</td>");
+	linhaTabela.append("<td>" + linha.editora + "</td>");
+	linhaTabela.append("<td>" + linha.publicacao + "</td>");
+	linhaTabela.append("<td>" + linha.resumo + "</td>");
+}
